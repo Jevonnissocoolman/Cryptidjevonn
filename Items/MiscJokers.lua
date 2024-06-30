a37269c2b9d24376c3ec026fe2547dad5eeaa4e1
Jevonnissocoolman <jevonnsivels2@gmail.com>
2024-06-29 20:01:40 -0700

    Add files via upload

--- Items/MiscJokers.lua
+++ Items/MiscJokers.lua
@@ -1987,89 +1987,6 @@ local bubblem_sprite = {
     px = 71,
     py = 95
 }
-local foodm = {
-    object_type = "Joker",
-    name = "cry-foodm",
-    key = "foodm",
-    config = {extra = {mult = 30, rounds_remaining = 2, text = "s"}, jolly = {t_mult = 8, type = 'Pair'}},
-    pos = {x = 0, y = 0},
-    loc_txt = {
-        name = 'Fast Food M',
-        text = {
-            "{C:mult}+#1#{} Mult",
-            "{C:red}self destructs{} after {C:attention}#2#{} round#3#,",
-            "Increased by {C:attention}1{} round when",
-            "{C:attention}Jolly Joker{} is {C:attention}sold{}",
-            "{C:inactive,s:0.8}2 McDoubles, 2 McChickens{}",
-            "{C:inactive,s:0.8}Large Fries, 20 Piece{}",
-            "{C:inactive,s:0.8}& Large Cake{}"
-        }
-    },
-    rarity = 1,
-    cost = 5,
-    discovered = true,
-    atlas = "foodm",
-    blueprint_compat = true,
-    loc_vars = function(self, info_queue, center)
-        info_queue[#info_queue+1] = { set = 'Joker', key = 'j_jolly', specific_vars = {self.config.jolly.t_mult, self.config.jolly.type} }
-        return {vars = {center.ability.extra.mult, center.ability.extra.rounds_remaining, center.ability.extra.text}}
-    end,
-    calculate = function(self, card, context)
-        if context.cardarea == G.jokers and (card.ability.extra.mult > 0) and not context.before and not context.after then
-            return {
-                message = localize{type='variable',key='a_mult',vars={card.ability.extra.mult}},
-                mult_mod = card.ability.extra.mult, 
-                colour = G.C.MULT
-            }
-        end
-        if context.end_of_round and not context.blueprint and not context.individual and not context.repetition and not context.retrigger_joker then
-            card.ability.extra.rounds_remaining = card.ability.extra.rounds_remaining - 1
-            if card.ability.extra.rounds_remaining > 0 then
-                return {
-                    message = {"-1 Round"},
-                    colour = G.C.FILTER
-                }
-            else
-                G.E_MANAGER:add_event(Event({
-                    func = function()
-                        play_sound('tarot1')
-                        card.T.r = -0.2
-                        card:juice_up(0.3, 0.4)
-                        card.states.drag.is = true
-                        card.children.center.pinch.x = true
-                        G.E_MANAGER:add_event(Event({trigger = 'after', delay = 0.3, blockable = false,
-                            func = function()
-                                G.jokers:remove_card(card)
-                                card:remove()
-                                card = nil
-                                return true;
-                            end
-                        }))
-                        return true
-                    end
-                }))
-                return {
-                    message = "M!"
-                }
-            end
-        end
-        if context.selling_card and context.card.ability.name == "Jolly Joker" and not context.blueprint and not context.retrigger_joker then
-            card.ability.extra.rounds_remaining = card.ability.extra.rounds_remaining + 1
-            card_eval_status_text(card, 'extra', nil, nil, nil, {message = localize{type = 'variable', key = 'a_rounds_remaining', vars = {card.ability.extra.rounds_remaining}}})
-            return {calculated = true}
-        end
-        if card.ability.extra.rounds_remaining == 1 then
-            card.ability.extra.text = ""
-        end
-    end
-}
-local foodm_sprite = {
-    object_type = "Atlas",
-    key = "foodm",
-    path = "j_cry_fastfoodm.png",
-    px = 71,
-    py = 95
-}
 local mstack = {
     object_type = "Joker",
     name = "cry-mstack",
@@ -2222,4 +2139,4 @@ return {name = "Misc. Jokers",
             end
 
         end,
-        items = {dropshot_sprite, maximized_sprite, potofjokes_sprite, queensgambit_sprite, whip_sprite, lucky_joker_sprite, cursor_sprite, pickle_sprite, cube_sprite, triplet_rhythm_sprite, booster_sprite, chili_pepper_sprite, compound_interest_sprite, big_cube_sprite, eternalflame_sprite, nice_sprite, sus_sprite, chad_sprite, waluigi_sprite, seal_the_deal_sprite, jimball_sprite, fspinner_sprite, krustytheclown_sprite, blurred_sprite, gardenfork_sprite, lightupthenight_sprite, nosound_sprite, antennastoheaven_sprite, hunger_sprite, weegaming_sprite, redbloon_sprite, apjoker_sprite, maze_sprite, unjust_dagger_sprite, monkey_dagger_sprite, pirate_dagger_sprite, mondrian_sprite, jollysus_sprite, bubblem_sprite, foodm_sprite, mstack_sprite, dropshot, maximized, potofjokes, queensgambit, wee_fib, compound_interest, whip, pickle, triplet_rhythm, booster, chili_pepper, lucky_joker, cursor, cube, big_cube, nice, sus, chad, jimball, waluigi, eternalflame, seal_the_deal, fspinner, krustytheclown, blurred, gardenfork, lightupthenight, nosound, antennastoheaven, hunger, weegaming, redbloon, apjoker, maze, unjust_dagger, monkey_dagger, pirate_dagger, mondrian, jollysus, bubblem, food_m, mstack,}}
+        items = {dropshot_sprite, maximized_sprite, potofjokes_sprite, queensgambit_sprite, whip_sprite, lucky_joker_sprite, cursor_sprite, pickle_sprite, cube_sprite, triplet_rhythm_sprite, booster_sprite, chili_pepper_sprite, compound_interest_sprite, big_cube_sprite, eternalflame_sprite, nice_sprite, sus_sprite, chad_sprite, waluigi_sprite, seal_the_deal_sprite, jimball_sprite, fspinner_sprite, krustytheclown_sprite, blurred_sprite, gardenfork_sprite, lightupthenight_sprite, nosound_sprite, antennastoheaven_sprite, hunger_sprite, weegaming_sprite, redbloon_sprite, apjoker_sprite, maze_sprite, unjust_dagger_sprite, monkey_dagger_sprite, pirate_dagger_sprite, mondrian_sprite, jollysus_sprite, bubblem_sprite, mstack_sprite, dropshot, maximized, potofjokes, queensgambit, wee_fib, compound_interest, whip, pickle, triplet_rhythm, booster, chili_pepper, lucky_joker, cursor, cube, big_cube, nice, sus, chad, jimball, waluigi, eternalflame, seal_the_deal, fspinner, krustytheclown, blurred, gardenfork, lightupthenight, nosound, antennastoheaven, hunger, weegaming, redbloon, apjoker, maze, unjust_dagger, monkey_dagger, pirate_dagger, jollysus, bubblem, mstack, mondrian,}}
