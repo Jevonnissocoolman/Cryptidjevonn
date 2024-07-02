f4d55ed842b392a60a1377bb05fabce5fa090f1c
Jevonnissocoolman <jevonnsivels2@gmail.com>
2024-07-01 20:02:11 -0700

    Update MiscJokers.lua

--- Items/MiscJokers.lua
+++ Items/MiscJokers.lua
@@ -2287,7 +2287,7 @@ local notebook = {
                 		if G.jokers.cards[i].ability.name == 'Jolly Joker' then jollycount = jollycount + 1 end
             		end
 				if jollycount > 0 then
-					if pseudorandom(('cry_notebook') * 3) < G.GAME.probabilities.normal/card.ability.extra.odds then
+					if 3 * pseudorandom('cry_notebook') < G.GAME.probabilities.normal/card.ability.extra.odds then
 						card.ability.extra.slot = card.ability.extra.slot + 1
 						return {
                     					card_eval_status_text(card, 'extra', nil, nil, nil, {
